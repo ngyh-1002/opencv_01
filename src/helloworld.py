883e489d23d1ae57a3278f876f4ee7d03df02f9a
ngyh-1002 <01049200297aa@gmail.com>
2025-07-28 15:10:55 +0900

    도형그리기
    도형그리기

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
 
-new_height = 300
-new_width =  300
-
-
 # 이미지를 불러오는 명령어
 image = cv2.imread('../img/like_lenna.png')
+
+# 이미지 높이, 폭 초기화
+# new_height = 300
+# new_width =  300
+height, width = image.shape[:2]
 
 # 이미지를 변환하는 명령어
 # image_small = cv2.resize(image, (100, 100))
 # image_big = cv2.resize(image, dsize =None, fx=2, fy=2)
 # image_fliped = cv2.flip(image, 0)
-image_fliped = cv2.flip(image, 1)
-
+# image_fliped = cv2.flip(image, 1)
+matrix = cv2.getRotationMatrix2D((width/2, height/2), 90, 1)
+result = cv2.warpAffine(image, matrix, (width, height))
 # 원본 크기의 배열을 만든뒤 이미지를 원본크기로 복원하는 명령어
 #dst = np.zeros((new_height, new_width), dtype = np.uint8)
 #dst = cv2.resize(image_small, (new_width, new_height), dst = dst)
@@ -23,8 +25,8 @@ image_fliped = cv2.flip(image, 1)
 # cv2.imshow('resized Image', dst)
 # cv2.imshow('Image Window', image_small)
 # cv2.imshow('Image Window', image_big)
-cv2.imshow('Image Window', image_fliped)
-
+# cv2.imshow('Image Window', image_fliped)
+cv2.imshow('Image Window', result)
 
 # 창을 유지시키는 명령어
 cv2.waitKey(0)
---
+++ src/shape.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+space = np.zeros((768, 1380), dtype = np.uint8)
+line_color = 255
+color = 255
+# space = cv2.line(space, (100, 100), (800, 400), line_color, 3, 1)
+# space2 = cv2.circle(space, (800, 400), 100, color, 4, 1)
+# space3 = cv2.rectangle(space, (500, 200), (800,400), color, 5, 1)
+# space4 = cv2. ellipse(space, (500, 300), (300, 200), 0, 45, 250, color, 4)
+obj1= np.array([[300, 500], [500, 500], [400,600], [200, 600]])
+obj2= np.array([[600, 500], [800, 500], [700, 600], [900, 600]])
+space5 = cv2.polylines(space, [obj1], True, color,3)
+space5 = cv2.fillPoly(space, [obj2], color, 1)
+
+# cv2.imshow('line', space)
+# cv2.imshow('line', space2)
+# cv2.imshow('line', space3)
+# cv2.imshow('line', space4)
+cv2.imshow('line', space5)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
