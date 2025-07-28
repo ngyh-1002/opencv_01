3bc508921004b0790d85d328415d420829786a5e
ngyh-1002 <01049200297aa@gmail.com>
2025-07-28 14:28:23 +0900

    이미지 확대
    이미지확대

--- src/helloworld.py
+++ src/helloworld.py
@@ -9,14 +9,18 @@ new_width =  300
 image = cv2.imread('../img/like_lenna.png')
 
 # 이미지를 변환하는 명령어
-image_small = cv2.resize(image, (100, 100))
+# image_small = cv2.resize(image, (100, 100))
+image_big = cv2.resize(image, dsize =None, fx=2, fy=2)
 
-dst = np.zeros((new_height, new_width), dtype = np.uint8)
-dst = cv2.resize(image_small, (new_width, new_height), dst = dst)
+# 원본 크기의 배열을 만든뒤 이미지를 원본크기로 복원하는 명령어
+#dst = np.zeros((new_height, new_width), dtype = np.uint8)
+#dst = cv2.resize(image_small, (new_width, new_height), dst = dst)
+
 # 이미지를 보여주는 명령어
 # cv2.imshow('Image Window', image)
-cv2.imshow('resized Image', dst)
+# cv2.imshow('resized Image', dst)
 # cv2.imshow('Image Window', image_small)
+cv2.imshow('Image Window', image_big)
 
 # 창을 유지시키는 명령어
 cv2.waitKey(0)
