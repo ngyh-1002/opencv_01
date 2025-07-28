7a0ca9da3cbf5a4420fcc502794fdde47c6d3e56
ngyh-1002 <01049200297aa@gmail.com>
2025-07-28 12:44:54 +0900

    이미지 축소
    이미지 축소

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,6 +1,15 @@
 import cv2
 
+# 이미지를 불러오는 명령어
 image = cv2.imread('../img/like_lenna.png')
-cv2.imshow('Image Window', image)
+
+# 이미지를 변환하는 명령어
+image_small = cv2.resize(image, (100, 100))
+
+# 이미지를 보여주는 명령어
+# cv2.imshow('Image Window', image)
+cv2.imshow('Image Window', image_small)
+
+# 창을 유지시키는 명령어
 cv2.waitKey(0)
 cv2.destroyAllWindows()
